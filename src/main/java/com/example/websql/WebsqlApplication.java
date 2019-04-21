a41c04e6fbe0061eec50ad352c0054dd47f88be9
yuexingxing <=>
2019-04-21 14:35:09 +0800

    1-增加异步任务，可用于发邮件、短信、消息推送等

--- src/main/java/com/example/websql/WebsqlApplication.java
+++ src/main/java/com/example/websql/WebsqlApplication.java
@@ -2,9 +2,14 @@ package com.example.websql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling//开启定时器
+//开启定时器
+@EnableScheduling
+//开启异步调用方法
+@EnableAsync
+
 @SpringBootApplication
 public class WebsqlApplication {
 
--- src/main/java/com/example/websql/tasks/TestTask.java
+++ src/main/java/com/example/websql/tasks/TestTask.java
@@ -14,13 +14,17 @@ public class TestTask {
     //定义每3秒执行任务
     @Scheduled(fixedRate = 3000)
     public void reportCunnentTime() {
-//        System.out.println("现在时间: " + dateFormat.format(new Date()));
+        System.out.println("现在时间: " + dateFormat.format(new Date()));
     }
 
     /**
      * cron表达式
      * 自动设置启动任务时间范围
+     * 可以引用 cron 表达式 并且 位数为：6位
+     * 例子如下： 4-40  *  *  *  *  ？
+     * 4-40 表示一位
      * http://cron.qqe2.com
+     * spring boot 是不支持年的
      */
     @Scheduled(cron = "4-40 * * * * ?")
     public void test() {
