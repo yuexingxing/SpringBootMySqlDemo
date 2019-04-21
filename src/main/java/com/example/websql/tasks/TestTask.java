7e9566f825e7316d29bc98fdb08b64205ac0ca86
yuexingxing <=>
2019-04-21 13:52:09 +0800

    1-定时任务增加cron表达式

--- src/main/java/com/example/websql/tasks/TestTask.java
+++ src/main/java/com/example/websql/tasks/TestTask.java
@@ -1,6 +1,5 @@
 package com.example.websql.tasks;
 
-
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -15,6 +14,16 @@ public class TestTask {
     //定义每3秒执行任务
     @Scheduled(fixedRate = 3000)
     public void reportCunnentTime() {
+//        System.out.println("现在时间: " + dateFormat.format(new Date()));
+    }
+
+    /**
+     * cron表达式
+     * 自动设置启动任务时间范围
+     * http://cron.qqe2.com
+     */
+    @Scheduled(cron = "4-40 * * * * ?")
+    public void test() {
         System.out.println("现在时间: " + dateFormat.format(new Date()));
     }
 }
